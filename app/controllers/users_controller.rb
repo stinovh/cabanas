5bc84063b857ab819f9ddcd0a91af3c2d3ae912e
Stijn <elstino@gmail.com>
2015-09-30 11:24:48 +0200

    user controller, refactoring views devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = Search.create(user_params)
+    redirect_to @user
   end
 
   def show
