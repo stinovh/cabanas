70c9e0e5c49a52648fe43abb1a4ce1cdc02ae07e
Stijn <elstino@gmail.com>
2015-10-01 10:05:46 +0200

    added user_id to properties

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929190801) do
+ActiveRecord::Schema.define(version: 20151001080059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20150929190801) do
     t.string   "name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "properties", ["user_id"], name: "index_properties_on_user_id", using: :btree
 
   create_table "searches", force: :cascade do |t|
     t.string   "country"
