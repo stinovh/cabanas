0ee8df36a84f6db26c04cdd803275f49d879b528
Stijn <elstino@gmail.com>
2015-10-02 14:43:05 +0200

    working booking search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -14,12 +14,35 @@ class SearchesController < ApplicationController
 
   def show
     @search = Search.find(params[:id])
-    @availabilities = Availability.search(@search).order("created_at DESC")
+    availabilities = Availability.search(@search).order("created_at DESC")
+    @final = show_date(availabilities, @search).uniq
   end
 
   private
 
   def search_params
     params.require(:search).permit(:country, :price_night, :capacity, :start_date, :end_date)
+  end
+
+  def show_date(availabilities, search)
+    final = []
+    temp = []
+    availabilities.each do |availability|
+      if availability.property.bookings.empty?
+        final << availability
+      else
+        availability.property.bookings.each do |booking|
+          (booking.start_date..booking.end_date).each do |date|
+            temp << false  if search_date_in_booking_date(date, search)
+          end
+        end
+      end
+      final << availability if !temp.include?(false)
+    end
+    return final
+  end
+
+  def search_date_in_booking_date(date, search)
+    (search.start_date..search.end_date).cover?(date)
   end
 end
--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,13 +1,13 @@
 class Availability < ActiveRecord::Base
   belongs_to :property
-  has_one :booking, through: :property
+  has_many :bookings, through: :property
 
    def self.search(query = {})
     # where(:title, query) -> This would return an exact match of the query
     search_scope = joins(:property)
     search_scope = search_scope.where("availabilities.capacity >= ?", "#{query.capacity}") if query.capacity.present?
-    search_scope = search_scope.where("start_date <= ?", "#{query.start_date}") if query.start_date.present?
-    search_scope = search_scope.where("end_date >= ?", "#{query.end_date}") if query.end_date.present?
+    search_scope = search_scope.where("availabilities.start_date <= ?", "#{query.start_date}") if query.start_date.present?
+    search_scope = search_scope.where("availabilities.end_date >= ?", "#{query.end_date}") if query.end_date.present?
     search_scope = search_scope.where("properties.country like ?", "#{query.country}") if query.country.present?
     search_scope = search_scope.where("properties.price_night <= ?", "#{query.price_night}") if query.price_night.present?
     search_scope
